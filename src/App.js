32d392a8ee44282392fbeae3b41714cb59e41ad0
rohitraie18 <rohitraee07@gmail.com>
2024-06-10 21:47:07 +0545

    first changes

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Search from './components/search/search';
+
+function App() {
+
+  const handleSearchChange = (searchData) => {
+    console.log(searchData);
+  }
+  return (
+    <div className="container">
+       <Search onSearchChange = {handleSearchChange}/> 
+    </div>
+  );
+}
+
+
+export default App;
